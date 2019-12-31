c5c56fafdec632954217ced3641ebbae31af16a4
Charlton Trezevant <ct@ctis.me>
2019-12-31 16:43:50 -0500

    Update main.go
    Change infinite sleep loop to for+select

--- main.go
+++ main.go
@@ -6,14 +6,12 @@ import (
 	"hkporter/api"
 	"hkporter/hk"
 	"hkporter/msg"
-	"time"
 )
 
-// TODO: clean up CLI interface and add monitor to check for
 func main() {
 	hkPin := flag.String("pin", "", "HomeKit pairing PIN")
 	dbPath := flag.String("dbpath", "./db", "State database path (optional)")
-	verbose := flag.Bool("v", false, "Enable HomeKit log output")
+	verbose := flag.Bool("v", false, "Enable HomeKit server debug output")
 
 	apiURI := flag.String("api", "http://localhost:80", "Porter API server URI")
 	apiKey := flag.String("key", "default", "Porter API key")
@@ -33,6 +31,6 @@ func main() {
 	go apiBroker.Start()
 
 	for {
-		time.Sleep(60 * time.Second)
+		select {}
 	}
 }
